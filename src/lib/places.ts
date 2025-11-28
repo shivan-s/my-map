c1146b70829a9630961f6f0be9dd99b7c1e1049b
Shivan Sivakumaran <me@shivan.xyz>
2025-11-29 10:24:50 +1300

    feat: adding more locations

--- src/lib/places.ts
+++ src/lib/places.ts
@@ -13,5 +13,23 @@ export const places: Place[] = [
 		body: 'Where NZ and Aus served in WW1',
 		lngLat: [26.27333224, 40.240999036],
 		visited: false
+	},
+	{
+		title: 'Everest Base Camp - South',
+		body: 'I am fasicated by people who climb Everest. I would like to see the memorial for the 1996 disaster',
+		lngLat: [86.859444, 28.007222],
+		visited: false
+	},
+	{
+		title: 'K2 Base Camp',
+		body: 'I am fasinated by people who climb K2; seeing a memorial',
+		lngLat: [76.4982363155381, 35.83096326788909],
+		visited: false
+	},
+	{
+		title: 'Ilavalai',
+		body: 'Where my parents grew up',
+		lngLat: [79.99260229491308, 9.798151912925748],
+		visited: true
 	}
 ] as const;
